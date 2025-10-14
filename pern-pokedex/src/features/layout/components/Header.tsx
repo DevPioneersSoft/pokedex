270ec004bef31afce142b3fc44cedd75e4cfe39f
Alex <alejandro.gomero@uabc.edu.mx>
2025-10-13 17:05:21 -0700

    avance 13/10/25

---
+++ pern-pokedex/src/features/layout/components/Header.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@mantine/core";
+import Sesion from "./Sesion";
+
+export default function Header() {
+    return (
+        <div className="mb-10">
+            <div className="grid grid-cols-2">
+                <div className="flex justify-center">
+                    <img
+                        src="/pokedex-logo.png"
+                        className="w-70 h-20"
+                    />
+                </div>
+                <div className="flex shadow-2xl p-2 rounded-2xl border-b-1 border-white grow">
+                    <div className="grow">
+                        <div className="flex space-x-10">
+                            <div>
+                                <Button color="yellow">
+                                    Mi equipo
+                                </Button>
+                            </div>
+                            <div className="flex space-x-4">
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <Sesion />
+                </div>
+            </div>
+        </div>
+    )
+}
