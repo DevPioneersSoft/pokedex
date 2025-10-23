d742c16fe97e50673f577ec04b7d6da494fe5d6c
AshleyVillasenor <80383053+AshleyVillasenor@users.noreply.github.com>
2025-10-23 15:51:13 -0700

    new

---
+++ pern-pokedex/src/error/Error.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type DataWithResponseInit<T = unknown> = {
+    type: "DataWithResponseInit",
+    data: T, 
+    init: ResponseInit
+}
+
+//function isDataWithResponseInit<T-unknown>(errorsToRecord:)
+
+export default function Error() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ pern-pokedex/src/routes/index.ts
@@ -0,0 +1,3 @@
+import App from "../App"
+import Pokedex from "../features/cuadricula/components/Pokedex"
+//import RutaProtegida from ".."
