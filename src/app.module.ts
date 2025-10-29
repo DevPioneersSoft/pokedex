7fb80849336d84cbb4fcc97bde3843bc82b1d5fa
smarin <smarin@uabc.edu.mx>
2025-10-28 23:04:34 -0700

    importar pokemons

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ImportarPokemonesModule } from './importar-pokemones/importar-pokemones.module';
+import { PrismaService } from './prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true
+    }),
+    PokemonModule,
+    ImportarPokemonesModule
+  ],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
