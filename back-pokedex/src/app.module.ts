8fd9c9894f01bce247e5501bd5a7c45983ab896a
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2025-10-28 17:28:56 -0700

    feat: ✨ Avance de la implementación de importar pokemones

--- back-pokedex/src/app.module.ts
+++ back-pokedex/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ImportarPokemonesModule } from './importar-pokemones/importar-pokemones.module';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [PokemonModule, ImportarPokemonesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    PokemonModule,
+    ImportarPokemonesModule
+  ],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule { }
--- back-pokedex/src/importar-pokemones/importar-pokemones.module.ts
+++ back-pokedex/src/importar-pokemones/importar-pokemones.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ImportarPokemonesService } from './importar-pokemones.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { ImportarPokemonesController } from './importar-pokemones.controller';
+import { ImportarPokemonesService } from './importar-pokemones.service';
 
 @Module({
   controllers: [ImportarPokemonesController],
-  providers: [ImportarPokemonesService],
+  providers: [ImportarPokemonesService, PrismaService],
 })
-export class ImportarPokemonesModule {}
+export class ImportarPokemonesModule { }
