e4736ca2c70ef8fc4998ee78898c03e3d9c95603
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2025-11-05 15:32:08 -0800

    ultimos cambios

--- back-pokedex/src/app.module.ts
+++ back-pokedex/src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { ImportarPokemonesModule } from './importar-pokemones/importar-pokemones.module';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { PrismaService } from './prisma/prisma.service';
-import { FavoritosDto } from './usuario/dto/favoritos.dto';
 import { UsuarioModule } from './usuario/usuario.module';
 // import { FavoritosService } from './favoritos/favoritos.service';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
@@ -33,7 +32,6 @@ import { LoggingInterceptor } from './shared/interceptores/logging.interceptor';
   controllers: [],
   providers: [
     PrismaService,
-    FavoritosDto,
     {
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor,
